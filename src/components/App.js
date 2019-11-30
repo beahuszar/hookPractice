48c9e518ecfdbbae99c602cc1aeb5307ec6c65a5
Huszar Beata <beata.huszar@adevinta.com>
2019-11-30 13:00:29 +0100

    Comments on useState hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
 
 const App = () => {
+  // Array destructuring
+  // resource => the "state variable" that holds the data
+  // setResource => function to use when we want to update its value
+  // useState => initial value for the resource variable
   const [resource, setResource] = useState("posts");
 
   return (
     <div>
       <div>
+        {/** setResource updates resource and the component is rerendered */}
         <button onClick={() => setResource("posts")}>Posts</button>
         <button onClick={() => setResource("todos")}>Todos</button>
       </div>
